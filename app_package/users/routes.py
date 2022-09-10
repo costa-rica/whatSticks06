cf38f6c6b344e3c802d0e8a8b6f6c9ae86b0e62e
costa-rica <53882857+costa-rica@users.noreply.github.com>
2022-09-10 07:24:25 +0300

    update functionatily: add user location to database

--- app_package/users/routes.py
+++ app_package/users/routes.py
@@ -3,11 +3,13 @@ from flask import render_template, url_for, redirect, flash, request, \
     abort, session, Response, current_app, send_from_directory
 import bcrypt
 from wsh_models import sess, Users, login_manager, Oura_token, Locations, \
-    Weather_history
+    Weather_history, User_location_day
 from flask_login import login_required, login_user, logout_user, current_user
 import requests
-from app_package.users.utils import oura_sleep_call, oura_sleep_db_add
+from app_package.users.utils import oura_sleep_call, oura_sleep_db_add, \
+    add_db_locations, weather_api_call, add_db_weather_hist, db_add_user_loc_day
 from sqlalchemy import func
+from datetime import datetime
 
 salt = bcrypt.gensalt()
 
@@ -38,19 +40,22 @@ def login():
         print('user for logging in:::', user)
         # verify password using hash
         password = formDict.get('password_text')
-        if password:
-            if bcrypt.checkpw(password.encode(), user.password):
-                print("match")
-                login_user(user)
-                flash('Logged in succesfully')
-                # next = request.args.get('next')
-                # if not is_safe_url(next):
-                #     return flask.abort(400)
-                return redirect(url_for('dash.dashboard'))
+        if user:
+            if password:
+                if bcrypt.checkpw(password.encode(), user.password):
+                    print("match")
+                    login_user(user)
+                    flash('Logged in succesfully')
+                    # next = request.args.get('next')
+                    # if not is_safe_url(next):
+                    #     return flask.abort(400)
+                    return redirect(url_for('dash.dashboard'))
+                else:
+                    print("does not match")
             else:
-                print("does not match")
+                print('No password ****')
         else:
-            print('No password ****')
+            flash('No user by that name')
 
         # if successsful login_something_or_other...
 
@@ -88,16 +93,30 @@ def account():
     existing_oura_token =sess.query(Oura_token, func.max(
         Oura_token.id)).filter_by(user_id=current_user.id).first()[0]
     
+    print('current_user stuff:')
+    print(current_user.lat, ", ", current_user.lon)
+
+    print('current_user.lat:', current_user.lat)
+    print(type(current_user.lat))
+
+    user = sess.query(Users).filter_by(id = current_user.id).first()
     
 
 
+    if user.lat == None or user.lat == '':
+        existing_coordinates = ''
+    else:
+        existing_coordinates = f'{user.lat}, {user.lon}'
+        
+    
     if existing_oura_token:
         oura_token = current_user.oura_token_id[-1].token
         existing_oura_token_str = str(existing_oura_token.token)
-
     else:
         oura_token = ''
         existing_oura_token_str = ''
+
+
 
     if request.method == 'POST':
         formDict = request.form.to_dict()
@@ -114,6 +133,9 @@ def account():
 
                 #--2)make oura api          
                 sleep_dict = oura_sleep_call(new_token = new_token)
+                if sleep_dict == 'Error with Token':
+                    flash('Error with Token')
+                    return redirect(url_for('users.account'))
 
                 #--3)store date in database
                 
@@ -134,13 +156,87 @@ def account():
 
                 return redirect(url_for('users.account'))
         
-        if formDict.get('location_text'):
+        if (formDict.get('location_text') != existing_coordinates) or (
+            formDict.get('location_text')!='' and existing_coordinates == ''):
+            print('****** Updating User location ******')
+            min_loc_distance_difference = 1000
+            if formDict.get('location_text') != '':# Location is not null needs update
+              
+                user.lat = formDict.get('location_text').split(',')[0]
+                user.lon = formDict.get('location_text').split(',')[1]
+                sess.commit()
 
-            current_user.lat = formDict.get('location_text').split(',')[0]
-            current_user.lon = formDict.get('location_text').split(',')[1]
-            sess.commit()
+                # location_search = sess.query(Locations).filter_by
 
+                #1) Check Locations table to see if user's location already exists or close enough (.1 degree)
+                locations_unique_list = sess.query(Locations).all()
+                for loc in locations_unique_list:
+                    lat_diff = abs(user.lat - loc.lat)
+                    lon_diff = abs(user.lon - loc.lon)
+                    loc_dist_diff = lat_diff + lon_diff
+                    print('** Differences **')
+                    print('lat_difference:', lat_diff)
+                    print('lon_diff:', lon_diff)
+
+                    if loc_dist_diff < min_loc_distance_difference:
+                        print('-----> loc_dist_diff is less than min required')
+                        min_loc_distance_difference = loc_dist_diff
+                        location_id = loc.id
+
+                if min_loc_distance_difference < .1:
+                    print('**** min_loc_distance diff < .1 ***** FIRED ***')
+                    # if already exists row for user_loc_day today and location id don't add
+                    search_user_loc_day = sess.query(User_location_day).filter_by(
+                        location_id = location_id,
+                        user_id = current_user.id,
+                        date = datetime.today().strftime('%Y-%m-%d')
+                    ).first()
+                    if search_user_loc_day:
+                        print('User_loc_day alreay has entry no need to add another.')
+                    else:
+                        #2a) Existing location is close enough
+                        # no api call needee
+                        # add User_location_day get the latest temperature
+                        # 1a) get today's weather history
+                        today_weather = sess.query(Weather_history).filter_by(
+                            location_id = location_id, 
+                            date = datetime.today().strftime('%Y-%m-%d')
+                        ).first()
+
+                        avgtemp_f = today_weather.avgtemp_f
+                        if today_weather:
+                            user_loc_day_id = db_add_user_loc_day(avgtemp_f, location_id)
+
+                        print('*** No Weather API call, Locatoin or weather histry added')
+
+                else:
+                    print('**** min_loc_distance difff did NOT fire *****')
+                    #2b) No existing location near enough
+                    # make weather api call to get location
+                    weather_dict = weather_api_call()
+
+                    #3) add row locations
+                    location_id = add_db_locations(weather_dict)
+                    print('Successfully added location_id: ', location_id)
+
+                    #4) Add to weather_hist row
+                    weather_hist_id = add_db_weather_hist(weather_dict, location_id)
+                    print('Successfully added weather_hist_id: ', weather_hist_id)
+
+                    avgtemp_f = weather_dict.get('forecast').get('forecastday')[0].get('day').get('avgtemp_f')
+                    user_loc_day_id = db_add_user_loc_day(avgtemp_f, location_id)
+                    print('Successfully added user_loc_day_id: ', user_loc_day_id)
+
+            else:# Blank out user lat and lon
+                user.lat = None
+                user.lon = None
+                sess.commit()
+                print('**** NO weather API call *****')
+
+            return redirect(url_for('users.account'))
+        else:
+            print('**** NOT update location ***')
             
     
     return render_template('account.html', page_name = page_name, email=email,
-         oura_token = oura_token)
+         oura_token = oura_token, location_coords = existing_coordinates)
--- app_package/users/utils.py
+++ app_package/users/utils.py
@@ -1,20 +1,27 @@
-from flask_login import login_required, login_user, logout_user, current_user
+from flask import current_app
+from flask_login import login_required, login_user, logout_user, \
+    current_user
 import json
 import requests
 from datetime import datetime
-from wsh_models import sess, Users, Locations, Weather_history, Oura_token, Oura_sleep_descriptions
+from wsh_models import sess, Users, Locations, Weather_history, \
+    Oura_token, Oura_sleep_descriptions, User_location_day
 
 
 def oura_sleep_call(new_token):
-    print('*********')
-    print('** Made Oura Ring Call ***')
-    print('******')
+    # print('*********')
+    # print('** Made Oura Ring Call ***')
+    # print('******')
     url_sleep='https://api.ouraring.com/v1/sleep?start=2020-03-11&end=2020-03-21?'
     response_sleep = requests.get(url_sleep, headers={"Authorization": "Bearer " + new_token})
-    print('status_code: ', response_sleep.status_code)
+    # print('status_code: ', response_sleep.status_code)
     sleep_dict = response_sleep.json()
-    print('len of response: ', len(sleep_dict))
-    return sleep_dict
+    # print('len of response: ', len(sleep_dict))
+    if response_sleep.status_code !='200':
+        print('*** Error With Token ****')
+        return 'Error with Token'
+    else:
+        return sleep_dict
 
 def oura_sleep_db_add(sleep_dict, oura_token_id):
     # Add oura dictionary response to database
@@ -24,10 +31,10 @@ def oura_sleep_db_add(sleep_dict, oura_token_id):
             bedtime_end = sleep_session.get('bedtime_end'),
             user_id = current_user.id).first()
         if not sleep_session_exists:
-            print('Keys in first sleep session: ', sleep_session.keys())
-            print('There are ', len(sleep_session.keys()), ' keys in this list')
+            # print('Keys in first sleep session: ', sleep_session.keys())
+            # print('There are ', len(sleep_session.keys()), ' keys in this list')
 
-# delete any dict element whose key is not in column list
+            # delete any dict element whose key is not in column list
             for element in list(sleep_session.keys()):
                 if element not in Oura_sleep_descriptions.__table__.columns.keys():
                     print('element to delete: ', element)
@@ -35,21 +42,118 @@ def oura_sleep_db_add(sleep_dict, oura_token_id):
                     del sleep_session[element]
 
 
-            # if sleep_session.get('hr_5min'):
-            #     del sleep_session['hr_5min']
-            #     del sleep_session['hypnogram_5min']
-            #     del sleep_session['rmssd_5min']
-            # if sleep_session.get('temperature_trend_deviation') or sleep_session.get('temperature_trend_deviation') ==0:
-            #     print('**** does this get fired ****')
-            #     del sleep_session['temperature_trend_deviation']
-            # if sleep_session.get('timestamp'):
-            #     del sleep_session['timestamp']
-            print('*** After Deleteion ***')
-            
-            print('There are ', len(sleep_session.keys()), ' keys in this list')
-            print(sleep_session.keys())
             sleep_session['user_id'] = current_user.id
             sleep_session['token_id'] = oura_token_id
-            new_sleep = Oura_sleep_descriptions(**sleep_session)
-            sess.add(new_sleep)
-            sess.commit()
+            #check if existing sleep bedtime_end exists if yes skip
+            existing_sleep_bedtime_end = sess.query(Oura_sleep_descriptions).filter_by(
+                user_id = current_user.id,
+                bedtime_end = sleep_session['bedtime_end']
+            ).first()
+            if not existing_sleep_bedtime_end:
+                new_sleep = Oura_sleep_descriptions(**sleep_session)
+                sess.add(new_sleep)
+                sess.commit()
+
+def weather_api_call():
+    print('***** weather_api_call Funct *****')
+    #1) get weatehr api token form config
+    api_token = [j  for i,j in current_app.config.items() if i=='WEATHER_API_KEY'][0]
+
+    #2) get location from current user
+    location_coords = f'{current_user.lat},{current_user.lon}'
+
+
+    #3) make weather call 
+    base_url = 'http://api.weatherapi.com/v1'
+    # current = '/current.json'
+    # astronomy = '/astronomy.json'
+    history = '/history.json'
+
+    payload = {}
+    payload['q'] = location_coords
+    payload['key'] = api_token
+
+    payload['dt'] = datetime.today().strftime('%Y-%m-%d')
+    # payload['end_dt'] = '2022-09-09'
+    payload['hour'] = 0
+
+    r_history = requests.get(base_url + history, params = payload)
+    print('r_history status code: ', r_history.status_code)
+    weather_dict = r_history.json()
+    # print(payload)
+    print('******')
+    print('weather_api dict')
+    print(weather_dict)
+    #4) return dictionary
+    return weather_dict
+
+def add_db_locations(weather_dict):
+    add_dict = {}
+    add_dict['city'] = weather_dict.get('location').get('name')
+    add_dict['region'] = weather_dict.get('location').get('region')
+    add_dict['country'] = weather_dict.get('location').get('country')
+    add_dict['lat'] = weather_dict.get('location').get('lat')
+    add_dict['lon'] = weather_dict.get('location').get('lon')
+    new = Locations(**add_dict)
+    sess.add(new)
+    sess.commit()
+    return new.id
+
+def add_db_weather_hist(weather_dict, location_id):
+    hist_forecast_list = weather_dict.get('forecast').get('forecastday')
+    
+    weather_hist_list=[]
+    for forecast in hist_forecast_list:
+        weather_hist_temp ={}
+        # Get location stuff
+
+        weather_hist_temp['city_location_name'] = weather_dict.get('location').get('name')
+        weather_hist_temp['region_name'] = weather_dict.get('location').get('region')
+        weather_hist_temp['country_name'] = weather_dict.get('location').get('country')
+        weather_hist_temp['lat'] = weather_dict.get('location').get('lat')
+        weather_hist_temp['lon'] = weather_dict.get('location').get('lon')
+        weather_hist_temp['tz_id'] = weather_dict.get('location').get('tz_id')
+        weather_hist_temp['location_id'] = location_id #needs location id*****
+        
+        #Get temperature stuff
+        weather_hist_temp['date']= forecast.get('date')
+        weather_hist_temp['maxtemp_f']= forecast.get('day').get('maxtemp_f')
+        weather_hist_temp['mintemp_f']= forecast.get('day').get('mintemp_f')
+        weather_hist_temp['avgtemp_f']= forecast.get('day').get('avgtemp_f')
+        weather_hist_temp['sunset']= forecast.get('astro').get('sunset')
+        weather_hist_temp['sunrise']= forecast.get('astro').get('sunrise')
+        weather_hist_list.append(weather_hist_temp)
+        new = Weather_history(**weather_hist_list[0])
+        sess.add(new)
+        sess.commit()
+
+        return new.id
+
+def db_add_user_loc_day(avgtemp_f, location_id):
+    print('***in db_add_user_loc_day ***')
+    # print(weather_dict)
+    #1) dict with stuff
+    add_dict = {}
+    #2) add these itmes
+        #user_id
+    add_dict['user_id'] = current_user.id
+        
+
+        #date
+    add_dict['date'] = datetime.today().strftime('%Y-%m-%d')
+        #local_time
+    add_dict['local_time'] = datetime.now().strftime("%H:%M:%S")
+        #avgtemp_f
+    # add_dict['avgtemp_f'] = weather_dict.get('forecast').get('forecastday').get('day').get('avgtemp_f')
+    add_dict['avgtemp_f'] = avgtemp_f
+        #row_tpe = 'user'
+    add_dict['row_type'] = 'user'
+        #locagtion_id
+    add_dict['location_id'] = location_id
+
+    new = User_location_day(**add_dict)
+    sess.add(new)
+    sess.commit()
+
+    return new.id
+
